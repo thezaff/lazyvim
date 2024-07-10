4ac997b5cc0b0494a501ddff75fcfe4d01c9ac23
Zafar Khodjaev <zaffenergizante@gmail.com>
2024-07-11 00:02:46 +0500

    zaff: config

---
+++ lua/plugins/oxocarbon.lua
@@ -0,0 +1,14 @@
+return {
+    { "nyoom-engineering/oxocarbon.nvim" },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "oxocarbon",
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent"
+            }
+        },
+    },
+}
