9abbb548c4a1df3e5c0e925f234892837f57ede5
Zafar Khodjaev <zaffenergizante@gmail.com>
2024-07-12 00:10:31 +0500

    zaff: config

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,16 @@
+return {
+  { "thezaff/oxocarbon.nvim", name = "oxocarbon" },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "oxocarbon",
+      -- transparent = true,
+      -- styles = {
+      --   sidebars = "transparent",
+      --   floats = "transparent",
+      -- },
+    },
+  },
+}
